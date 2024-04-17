fb5aa49a8f544c0b4557893ad654a0a0a0cdcdf8
Shumaf B <shumafbatov.dev@gmail.com>
2024-04-17 09:42:58 +0300

    App init

---
+++ src/cards/ReportCard.vue
@@ -0,0 +1,10 @@
+<template>
+    <a-card title="Card Title">
+      <template #extra><a href="#">more</a></template>
+
+    </a-card>
+</template>
+<script lang="ts" setup>
+import { Card as ACard } from 'ant-design-vue';
+
+</script>
---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <ReportCard />
+  </div>
+</template>
+<script lang="ts" setup>
+import ReportCard from '../cards/ReportCard.vue'
+</script>
